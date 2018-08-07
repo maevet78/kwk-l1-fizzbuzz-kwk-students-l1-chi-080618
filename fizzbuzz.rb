9f71f13906fef8036b090be0e706bfb758bc13f7
maevet78 <190439mt@saintviator.com>
2018-08-07 19:31:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,29 @@
-def fizzbuzz (number)
-  if number % 15 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0 
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
-  else 
-    nil
-  end
-end
+# def fizzbuzz (number)
+# if number % 15 == 0
+#   "FizzBuzz"
+# elsif number % 3 == 0 
+#   "Fizz"
+# elsif number % 5 == 0
+#   "Buzz"
+# else 
+#   "```````nil"
+# end
+# end
+# puts fizzbuzz (30)
 
-puts fizzbuzz (30)
+def fizzbuzz(number)
+  if number % 5 == 0 && number % 3 == 0 
+    "Fizzbuzz"
+  elsif number % 5 == 0 && number % 3 != 0
+    "Buzz"
+  elsif number % 5 != 0 && number % 3 == 0
+    "Fizz"
+  else 
+    "nil"
+  end 
+end 
+
+puts fizzbuzz(3)
+puts fizzbuzz(5)
+puts fizzbuzz(15)
+puts fizzbuzz(1)
